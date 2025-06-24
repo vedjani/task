17e0919273559e5983ed545b0fdefc941d7d12a8
vedjani <139037017+vedjani@users.noreply.github.com>
2025-06-24 11:57:16 +0530

    commit

--- module -c programing/19-6 task b/task1.c
+++ module -c programing/19-6 task b/task1.c
@@ -1,38 +1,30 @@
-
-// ✅ Prime Number Check Karne ki Steps:
-// User se number lo.
-// Check karo ki wo number 1 hai ya nahi.
-// Agar 1 hai to wo Prime nahi hota.
-// Check karo ki wo number 2 se lekar (number - 1) tak kisi number se divide hota hai ya nahi.
-// Agar divide hota hai to Prime nahi hai.
-// Agar kisi se divide nahi hota to Prime hai.
 #include<stdio.h>
 #include<conio.h>
 int main()
 {
-    int num,one=1;
+    int num,count=0;
 
-    if (num<=1)
+    printf("enter number :- ");
+    scanf("%d",&num);
+
+    for (int i=1;i<=num;i++)
     {
-        one=0;
+        if (num%i==0)
+        {
+            count++;
+        }
+
+    }
+    if (count>2)
+    {
+        printf("not prime");
     }
     else
     {
-        for (int i = 2; i < num; i++)
-        {
-            if (num%i==0)
-            {
-                printf("this number is prime");
-            }
-            else
-            {
-                printf("this is not prime");
-            }
-            
-            
-        }
-        
+        printf("prime");
     }
+    
+    
     
     
     
