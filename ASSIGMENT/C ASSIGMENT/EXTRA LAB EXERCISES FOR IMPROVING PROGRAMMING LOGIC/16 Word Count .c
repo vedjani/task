9fffcc123e199a8a5debfe8413cadd9d9d0ca1b4
vedjani <139037017+vedjani@users.noreply.github.com>
2025-09-06 14:35:52 +0530

    commit

--- ASSIGMENT/C ASSIGMENT/EXTRA LAB EXERCISES FOR IMPROVING PROGRAMMING LOGIC/16 Word Count .c
+++ ASSIGMENT/C ASSIGMENT/EXTRA LAB EXERCISES FOR IMPROVING PROGRAMMING LOGIC/16 Word Count .c
@@ -1,27 +1,43 @@
 // Write a C program that counts the number of words in a sentence entered by the user. 
 // Challenge: Modify the program to find the longest word in the sentence.
 #include<stdio.h>
-#include<conio.h>
 #include<string.h>
+
 int main()
 {
-    char str[500],big[500],st;
-    int spc=0,num=0,count=0,ncount=0;
+    char str[100], biggest[100], temp[100];
+    int spc = 0, num = 0, maxlen = 0, j = 0;
 
-    printf("enter a sentence:- ");
+    printf("Enter string: ");
     gets(str);
 
-    num=strlen(str);
-    for (int i = 0; i<=num; i++)
-    {   
-        
-        if(str[i]==' ')
-        spc++;
-    }
-    spc++;
-        
-    printf("word is %d",spc);
+    num = strlen(str);
+   
 
-    
+    for (int i = 0; i <= num; i++)
+    {
+        if (str[i] != ' ' && str[i] != '\0') 
+        {
+            temp[j++] = str[i];
+        }
+        else 
+        {
+            temp[j] = ' ';
+            if (j > 0)
+            {
+                if (j > maxlen) 
+                {
+                    maxlen = j;
+                    strcpy(biggest, temp);
+                }
+                spc++; 
+            }
+            j = 0; 
+        }
+    }
+
+    printf("\n%d words in this sentence", spc);
+    printf("\nlongest string is: %s ", biggest);
+
     return 0;
 }
